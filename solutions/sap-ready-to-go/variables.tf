1bdc49b53c1d44172f3f9981c1ebeedb08cdefc5
surajsbharadwaj <suraj.bharadwaj@ibm.com>
2023-09-07 11:47:23 +0200

    fix: create_seperate_fs var description

--- solutions/sap-ready-to-go/variables.tf
+++ solutions/sap-ready-to-go/variables.tf
@@ -73,7 +73,7 @@ variable "os_image_distro" {
 #####################################################
 
 variable "powervs_create_separate_fs_share" {
-  description = "Deploy separate IBM PowerVS instance(0.5 cpus, 2 GB memory size, shared processor on s922.) as central file system share. All filesystems defined in 'powervs_share_storage_config' optional variable will be NFS exported and mounted on HANA and NW PowerVS instances."
+  description = "Deploy separate IBM PowerVS instance(0.5 cpus, 2 GB memory size, shared processor on s922.) as central file system share. All filesystems defined in 'powervs_share_storage_config' optional variable will be NFS exported and mounted on Netweaver PowerVS instances."
   type        = bool
 }
 
