d2b06c55a18379da3ed1e50c4cc11f2128113959
surajsbharadwaj <suraj.bharadwaj@ibm.com>
2024-04-05 12:56:11 +0200

    feat: switch lon06 DC to PER DC SKIP UPGRADE TEST: skipping because of provider issue

--- solutions/e2e/main.tf
+++ solutions/e2e/main.tf
@@ -7,7 +7,7 @@
 
 module "fullstack" {
   source  = "terraform-ibm-modules/powervs-infrastructure/ibm//modules/powervs-vpc-landing-zone"
-  version = "4.8.0"
+  version = "4.9.0"
 
   providers = { ibm.ibm-is = ibm.ibm-is, ibm.ibm-pi = ibm.ibm-pi }
 
