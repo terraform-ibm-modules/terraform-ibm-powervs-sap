5da9e0f97b62c6f4ea67f79ca9d2c783a63982fd
surajsbharadwaj <suraj.bharadwaj@ibm.com>
2024-06-07 13:30:49 +0200

    feat: hana storage calculation update factor according to latest doc 2024 SKIP UPGRADE TEST: version change

--- modules/pi-hana-storage-config/main.tf
+++ modules/pi-hana-storage-config/main.tf
@@ -6,23 +6,23 @@ locals {
   memory_size = tonumber(element(split("x", var.pi_hana_instance_sap_profile_id), 1))
   auto_cal_storage_config = {
     "memory_lt_900" = {
-      shared_disk = { tier = "tier3", count = "1", size = local.memory_size },                                                 #3 IOPS/GB
-      log_disk    = { tier = "tier5k", count = "4", size = ceil((local.memory_size / 2) / 4) },                                #fixed 5k iops
-      data_disk   = { tier = "tier0", count = "4", size = local.memory_size < 256 ? 77 : ceil((local.memory_size * 1.2) / 4) } #25 IOPS/GB
+      shared_disk = { tier = "tier3", count = "1", size = local.memory_size },                                                          #3 IOPS/GB
+      log_disk    = { tier = "tier5k", count = "4", size = ceil((local.memory_size / 2) / 4) },                                         #fixed 5k iops
+      data_disk   = { tier = "tier0", count = "4", size = local.memory_size < 256 ? 81 : floor((local.memory_size * 1.5 * 1.074) / 4) } #25 IOPS/GB
     },
-    "memory_bt_900_2100" = {
+    "memory_bt_900_1850" = {
       shared_disk = { tier = "tier3", count = "1", size = 1000 }, #3 IOPS/GB
       log_disk    = { tier = "tier0", count = "4", size = 128 },  #25 IOPS/GB
-      data_disk   = { tier = "tier3", count = "4", size = 648 }   #3 IOPS/GB
+      data_disk   = { tier = "tier3", count = "4", size = 670 }   #3 IOPS/GB
     },
     "memory_gt_2100" = {
-      shared_disk = { tier = "tier3", count = "1", size = 1000 },                                #3 IOPS/GB
-      log_disk    = { tier = "tier0", count = "4", size = 128 },                                 #25 IOPS/GB
-      data_disk   = { tier = "tier3", count = "4", size = floor((local.memory_size * 1.2) / 4) } #3 IOPS/GB
+      shared_disk = { tier = "tier3", count = "1", size = 1000 },                                        #3 IOPS/GB
+      log_disk    = { tier = "tier0", count = "4", size = 128 },                                         #25 IOPS/GB
+      data_disk   = { tier = "tier3", count = "4", size = floor((local.memory_size * 1.5 * 1.074) / 4) } #3 IOPS/GB
     }
   }
 
-  storage_config = local.memory_size < 900 ? local.auto_cal_storage_config["memory_lt_900"] : local.memory_size > 2100 ? local.auto_cal_storage_config["memory_gt_2100"] : local.auto_cal_storage_config["memory_bt_900_2100"]
+  storage_config = local.memory_size < 900 ? local.auto_cal_storage_config["memory_lt_900"] : local.memory_size > 1850 ? local.auto_cal_storage_config["memory_gt_2100"] : local.auto_cal_storage_config["memory_bt_900_1850"]
 
   auto_cal_hana_storage_config = [
     {
