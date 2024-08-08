8fbb2091945f6040af91da9475a752ae0d66476f
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-08-08 15:31:52 +0100

    chore(deps): update required_provider to latest for deployable modules / architectures (#703)
    * chore(deps): update required_provider to latest for deployable modules / architectures
    
    * test: changed datacenter to tor01 due to issues in sao04
    
    ---------
    
    Co-authored-by: ludwig-mueller <ludwig.mueller@ibm.com>

--- solutions/e2e/version.tf
+++ solutions/e2e/version.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "= 1.67.1"
+      version = "1.68.0"
     }
     time = {
       source  = "hashicorp/time"
-      version = "= 0.11.2"
+      version = "0.12.0"
     }
   }
 }
