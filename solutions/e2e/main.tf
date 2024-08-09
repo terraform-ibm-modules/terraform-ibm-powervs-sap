2a44fa437df1406a315cea6b198f65f407dcb6e1
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-08-10 00:08:47 +0100

    chore(deps): update terraform terraform-ibm-modules/powervs-infrastructure/ibm to v6 (#706)
    * chore(deps): update terraform terraform-ibm-modules/powervs-infrastructure/ibm to v6
    
    * test: changed datacenter to tor01 due to issues in sao04
    
    * fix: add breaking change from powervs-infrastructure module
    BREAKING CHANGE: from PowerVS Infrastructure module: recreate nfs share
    
    ---------
    
    Co-authored-by: ludwig-mueller <ludwig.mueller@ibm.com>

--- solutions/e2e/main.tf
+++ solutions/e2e/main.tf
@@ -7,7 +7,7 @@
 
 module "powervs_infra" {
   source  = "terraform-ibm-modules/powervs-infrastructure/ibm//modules/powervs-vpc-landing-zone"
-  version = "5.3.0"
+  version = "6.0.0"
 
   providers = { ibm.ibm-is = ibm.ibm-is, ibm.ibm-pi = ibm.ibm-pi, ibm.ibm-sm = ibm.ibm-sm }
 
