572e374f710d90330e14d5aa74d616ef13dd75ac
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-04-19 06:18:47 +0000

    chore(deps): update terraform terraform-ibm-modules/powervs-infrastructure/ibm to v98

--- solutions/e2e/main.tf
+++ solutions/e2e/main.tf
@@ -7,7 +7,7 @@
 
 module "powervs_infra" {
   source  = "terraform-ibm-modules/powervs-infrastructure/ibm//modules/powervs-vpc-landing-zone"
-  version = "8.3.0"
+  version = "98.0.0"
 
   providers = { ibm.ibm-is = ibm.ibm-is, ibm.ibm-pi = ibm.ibm-pi, ibm.ibm-sm = ibm.ibm-sm }
 
