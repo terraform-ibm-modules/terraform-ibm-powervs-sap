2c73a885820a3b981f2c7a9ab7f489a60b0bc624
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-05-31 11:27:29 +0000

    chore(deps): update terraform dependencies

--- solutions/e2e/main.tf
+++ solutions/e2e/main.tf
@@ -7,7 +7,7 @@
 
 module "standard" {
   source  = "terraform-ibm-modules/powervs-infrastructure/ibm//modules/powervs-vpc-landing-zone"
-  version = "8.4.4"
+  version = "8.4.5"
 
   providers = { ibm.ibm-is = ibm.ibm-is, ibm.ibm-pi = ibm.ibm-pi, ibm.ibm-sm = ibm.ibm-sm }
 
