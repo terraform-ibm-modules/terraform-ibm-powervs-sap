fada54de38e55bfbb473ee7069ac5a7bfbe6ae32
stafaniasaju <stafania.saju@ibm.com>
2025-06-10 12:54:11 +0200

    chore: update powervs-vpc-landing-zone to v8.5.0

--- solutions/e2e/main.tf
+++ solutions/e2e/main.tf
@@ -7,7 +7,7 @@
 
 module "standard" {
   source  = "terraform-ibm-modules/powervs-infrastructure/ibm//modules/powervs-vpc-landing-zone"
-  version = "8.4.5"
+  version = "8.5.0"
 
   providers = { ibm.ibm-is = ibm.ibm-is, ibm.ibm-pi = ibm.ibm-pi, ibm.ibm-sm = ibm.ibm-sm }
 
