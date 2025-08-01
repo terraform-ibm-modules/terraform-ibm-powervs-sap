e29071579a7662218ea3b7e06167679862717421
ludwig-mueller <ludwig.mueller@ibm.com>
2025-08-01 15:31:50 +0200

    chore(deps): upgrade tf provider to 1.81.0

--- solutions/e2e/version.tf
+++ solutions/e2e/version.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.80.4"
+      version = "1.81.0"
     }
     time = {
       source  = "hashicorp/time"
