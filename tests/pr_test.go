c869c574438ea2b67ec57ec613b956fdd02d9d6c
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-09-01 16:15:36 +0530

    chore(deps): update dependencies (#865)
    * chore(deps): update dependencies
    
    * chore: change main test to wdc06
    
    ---------
    
    Co-authored-by: ludwig-mueller <ludwig.mueller@ibm.com>

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -75,7 +75,7 @@ func TestRunBranchExample(t *testing.T) {
 
 func TestRunMainExample(t *testing.T) {
 	t.Parallel()
-	options := setupOptions(t, "m", "us-south")
+	options := setupOptions(t, "m", "wdc06")
 
 	output, err := options.RunTestUpgrade()
 	if !options.UpgradeTestSkipped {
