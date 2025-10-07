d90bc6f61a09328d48f627be22b289d551fa333b
Trilochan Pandey <tripan56@in.ibm.com>
2025-10-07 22:25:47 +0530

    Update modules/pi-sap-system-type1/main.tf
    Co-authored-by: Suraj Bharadwaj <101711050+surajsbharadwaj@users.noreply.github.com>

--- modules/pi-sap-system-type1/main.tf
+++ modules/pi-sap-system-type1/main.tf
@@ -32,7 +32,7 @@ module "pi_hana_instance" {
   pi_instance_name           = local.pi_hana_instance_name
   pi_ssh_public_key_name     = var.pi_ssh_public_key_name
   pi_image_id                = var.pi_hana_instance.image_id
-  pi_networks                = local.pi_network
+  pi_networks                = local.pi_networks
   pi_sap_profile_id          = var.pi_hana_instance.sap_profile_id
   pi_boot_image_storage_tier = "tier3"
   pi_storage_config          = module.pi_hana_storage_calculation.pi_hana_storage_config
