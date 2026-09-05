aa3aa64827c623a30283060e9995b0f88f6941af
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-09-05 07:25:49 +0000

    chore(deps): update dependencies

--- solutions/ibm-catalog/sap-ready-to-go/main.tf
+++ solutions/ibm-catalog/sap-ready-to-go/main.tf
@@ -6,7 +6,7 @@
 
 module "standard" {
   source  = "terraform-ibm-modules/powervs-infrastructure/ibm//modules/powervs-vpc-landing-zone"
-  version = "11.3.0"
+  version = "11.4.0"
 
   providers = {
     ibm.ibm-is = ibm.ibm-is
